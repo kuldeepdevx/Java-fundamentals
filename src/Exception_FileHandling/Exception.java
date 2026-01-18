80e050ffd87d9732ddaef98e517d717666bbe251
kuldeepdevx <kuldeep.girase.work@gmail.com>
2026-01-18 23:47:22 +0530

    Add Exception example

--- src/Exception_FileHandling/Exception.java
+++ src/Exception_FileHandling/Exception.java
@@ -3,6 +3,8 @@ package Exception_FileHandling;
 public class Exception {
     public static void main(String[] args){
 
+        int y=5/0;
+
         try {
             int x = 10 / 0;
         } catch (ArithmeticException e) {
